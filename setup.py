9023388e88fbf8cd147ede1480f9f8e5c1d592e3
Zorn <marvinzorn23@gmail.com>
2024-07-03 15:33:13 +0200

    add dependencies

--- setup.py
+++ setup.py
@@ -2,13 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='decomply',
-    version='1.1.0',
+    version='1.1.1',
     packages=find_packages(),
     description='decomply allows unified processing of nested dictionaries',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',  # for Markdown
     author='TrueKenji',
     url='https://github.com/truekenji/decomply',
+    install_requires=[
+        'numpy',  # Replace with the actual name and version of your dependency
+    ],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
